08979cff1c2127613818d8c4b267be9e3739fcd3
Karol Banat <karol.banat@gmail.com>
2023-12-19 12:12:39 +0100

    Header content outline

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,16 @@
 
 <header class="wrapper-grid">
 	<Navigation />
+	<div>
+		<div class="flow">
+			<h1>Body Mass Index Calculator</h1>
+			<p>
+				Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is
+				not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall
+				health and well-being.
+			</p>
+		</div>
+	</div>
 </header>
 <main></main>
 
