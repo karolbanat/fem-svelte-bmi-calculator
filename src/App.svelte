e6977e558466826cbb1361e308cd72bcc1c7f17a
Karol Banat <karol.banat@gmail.com>
2023-12-19 12:07:55 +0100

    Done navigation

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import Navigation from './lib/Navigation.svelte';
 </script>
 
+<header class="wrapper-grid">
+	<Navigation />
+</header>
 <main></main>
 
 <style>
---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,30 @@
+<script lang="ts"></script>
+
+<nav>
+	<a href="#" aria-label="Home page">
+		<img src="/assets/logo.svg" alt="" />
+	</a>
+</nav>
+
+<style>
+	nav {
+		padding-block-start: clamp(var(--spacer-rem-400), 7vw - 1.75rem, var(--spacer-rem-900));
+		padding-block-end: clamp(var(--spacer-rem-300), 5.25vw, var(--spacer-rem-500));
+	}
+
+	a {
+		display: inline-block;
+	}
+
+	@media (max-width: 62em) {
+		nav {
+			text-align: center;
+		}
+	}
+
+	@media (min-width: 62em) {
+		nav {
+			padding-block-end: var(--spacer-rem-300);
+		}
+	}
+</style>
