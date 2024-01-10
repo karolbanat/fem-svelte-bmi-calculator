1058247badf06dbb066e5e64e1d58a09ac5fab0a
Karol Banat <karol.banat@gmail.com>
2024-01-10 11:49:22 +0100

    Tip cards and layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import iconEating from '/src/assets/icons/icon-eating.svg';
+	import iconExercise from '/src/assets/icons/icon-exercise.svg';
+	import iconSleep from '/src/assets/icons/icon-sleep.svg';
+
 	import BMIForm from './lib/BMIForm.svelte';
 	import HeaderBox from './lib/HeaderBox.svelte';
 	import Navigation from './lib/Navigation.svelte';
+	import TipCard from './lib/cards/TipCard.svelte';
 </script>
 
 <header class="wrapper-grid">
@@ -19,7 +24,7 @@
 	</div>
 </header>
 <main class="wrapper-grid">
-	<section class="meaning-section even-columns-screen even-columns-screen--medium vertical-end-grid">
+	<section class="meaning-section | even-columns-screen even-columns-screen--medium vertical-end-grid | section-spacer">
 		<img
 			src="/assets/images/image-man-eating.webp"
 			alt="Smiling man holding chopsticks with sushi roll."
@@ -31,6 +36,27 @@
 			message="A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week."
 			paddedLeft={true}
 		/>
+	</section>
+
+	<section class="tips-section | section-spacer">
+		<h2 class="visually-hidden">Tips</h2>
+		<div class="even-columns-screen" style="--even-columns-screen-spacer: var(--spacer-rem-400);">
+			<TipCard
+				icon={iconEating}
+				title="Healthy eating"
+				text="Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood."
+			/>
+			<TipCard
+				icon={iconExercise}
+				title="Regular exercise"
+				text="Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity."
+			/>
+			<TipCard
+				icon={iconSleep}
+				title="Adequate sleep"
+				text="Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation."
+			/>
+		</div>
 	</section>
 </main>
 
@@ -53,7 +79,12 @@
 	}
 
 	.meaning-section {
-		margin-block: clamp(4.5rem, 12.5rem, 6rem);
+		margin-block-start: var(--section-spacer);
+	}
+
+	.tips-section {
+		padding-block-start: clamp(var(--spacer-rem-700), 7.5vw, var(--spacer-rem-900));
+		padding-block-end: clamp(var(--spacer-rem-700), 7.5vw, 6rem);
 	}
 
 	@media (min-width: 42em) and (max-width: 62em) {
---
+++ src/lib/cards/TipCard.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let icon: string;
+	export let title: string = '';
+	export let text: string = '';
+</script>
+
+<article>
+	<img src={icon} alt="" class="card-icon" />
+	<div class="card-body">
+		<h3>{title}</h3>
+		<p>{text}</p>
+	</div>
+</article>
+
+<style>
+	article {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: clamp(var(--spacer-rem-400), 5vw, var(--spacer-rem-600));
+	}
+
+	h3 {
+		font-size: var(--fz-600);
+		line-height: 1.1;
+	}
+
+	.card-body {
+		display: grid;
+		gap: var(--spacer-rem-300);
+	}
+
+	@media (min-width: 36em) and (max-width: 56em) {
+		article {
+			flex-direction: row;
+			align-items: center;
+		}
+	}
+</style>
